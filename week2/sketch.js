af25b3187428e732e1cbddaa34b64188e37d874e
Sylvan Zheng <squidgetx@gmail.com>
2020-09-30 23:32:47 -0400

    week2

---
+++ week2/sketch.js
@@ -0,0 +1,100 @@
+/* global createCanvas, mouseIsPressed, fill, mouseX, mouseY, ellipse */
+
+let audioContext;
+let spectrum = [];
+let spectralCentroid = 0;
+let spectralFlatness = 0;
+let FFTSIZE = 256
+let analyzer, mic;
+
+function createAudioCtx() {
+    let AudioContext = window.AudioContext || window.webkitAudioContext;
+    return new AudioContext();
+}
+
+function createMicSrcFrom(audioCtx) {
+    return new Promise((resolve, reject) => {
+        /* get microphone access */
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then((stream) => {
+            /* create source from microphone input stream */
+            let src = audioCtx.createMediaStreamSource(stream);
+            resolve(src);
+        }).catch((err) => {
+            reject(err)
+        });
+    });
+}
+
+function callback(features) {
+    spectrum = features.amplitudeSpectrum
+    spectralCentroid = features.spectralCentroid
+    spectralFlatness = Math.pow(features.spectralFlatness, 0.3) * 1.2
+}
+
+function setupMeydaAnalzer(ctx) {
+    createMicSrcFrom(ctx).then((src) => {
+        analyzer = Meyda.createMeydaAnalyzer({
+            'audioContext': ctx,
+            'source': src,
+            'bufferSize': FFTSIZE,
+            'featureExtractors': [
+              "amplitudeSpectrum",
+              "spectralCentroid",
+              "spectralFlatness",
+            ],
+            'callback': callback
+        });
+        analyzer.start();
+    }).catch((err) => {
+        alert(err);
+    })
+}
+
+function setupAudio() {
+    console.log("Setting up audio")
+    userStartAudio();
+    let ctx = getAudioContext()
+    setupMeydaAnalzer(ctx)
+
+}
+
+function setup() {
+    // mimics the autoplay policy
+
+    createCanvas(1024, 1024);
+
+}
+
+function draw() {
+    background(color('rgba(255, 250, 240, 0.1)'));
+    plot(spectrum);
+}
+
+function plot(spectrum) {
+    fill('rgba(0,0,0,0)');
+    strokeWeight(2)
+    rectMode(RADIUS)
+    for (let i = spectrum.length - 1; i >= 0; i--) {
+        //for (let i = 0; i < spectrum.length; i++) {
+        push()
+        let b = map(255 * spectrum[i], 0, 255, 1, 0)
+        let hue = map(spectralCentroid, 0, FFTSIZE / 2, 360, 60).toFixed(0)
+        let l = map(i, 0, spectrum.length, 50, 100)
+        let c = color(`hsla(${hue}, ${l}%, 80%, ${(1-b).toFixed(0)})`)
+        stroke(c)
+        translate(width / 2, height / 2)
+        rotate(frameCount / 100)
+        let scale = i * 4
+        rect(0, 0, scale, scale, spectralFlatness * scale)
+        pop()
+        //text(spectrum[i].toFixed(2), i * 10, i * 10)
+    }
+
+}
+
+function mousePressed() {
+    userStartAudio();
+    setupAudio();
+}
